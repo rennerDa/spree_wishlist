0a23e0831bae17050afa48c344039fde5d87e230
Daniel Renner <renner.da@gmail.com>
2013-12-04 01:06:09 +0100

    Added possibility to add every product inside a wishlist to the cart (quantity 1).

--- app/controllers/spree/wishlists_controller.rb
+++ app/controllers/spree/wishlists_controller.rb
@@ -1,7 +1,7 @@
 class Spree::WishlistsController < Spree::StoreController
   helper 'spree/products'
 
-  before_filter :find_wishlist, :only => [:destroy, :show, :update, :edit]
+  before_filter :find_wishlist, :only => [:destroy, :show, :update, :edit, :add_all_products_to_cart]
 
   respond_to :html
   respond_to :js, :only => [:update]
@@ -53,6 +53,22 @@ class Spree::WishlistsController < Spree::StoreController
     respond_with(@wishlist )do |format|
       format.html { redirect_to account_path }
     end
+  end
+
+  def add_all_products_to_cart
+    populator = Spree::OrderPopulator.new(current_order(true), current_currency)
+    @wishlist.wished_products.each do |wish|
+      variant = wish.variant
+      product = variant.product
+
+      if populator.populate({:variants => {variant.id => 1}})
+        current_order.ensure_updated_shipments
+      else
+        flash[:error] = populator.errors.full_messages.join(" ")
+        redirect_to :back
+      end
+    end
+    redirect_to cart_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Spree::Core::Engine.routes.draw do
-  resources :wishlists
+  resources :wishlists do
+  	member do
+    	get :add_all_products_to_cart
+    end 
+  end
   resources :wished_products, only: [:create, :update, :destroy]
   get '/wishlist' => "wishlists#default", as: "default_wishlist"
 end
